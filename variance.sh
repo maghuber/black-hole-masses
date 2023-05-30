c727df8e4b521a1bdace8b097eb95e4eb88772f7
maghuber <maggie.c.huber@gmail.com>
2023-05-30 10:38:08 -0600

    specified user variable

--- variance.sh
+++ variance.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=mahu8801@colorado.edu
 #SBATCH --qos=normal	      		
 #SBATCH --partition=amilan		
-#SBATCH --output=/scratch/alpine/$USER/output/python_%j.out
+#SBATCH --output=/scratch/alpine/mahu8801/output/python_%j.out
 
 # Load the python module
 module load python
@@ -12,5 +12,5 @@ module load anaconda
 conda activate blackhole_env
 
 # Run Python Script
-cd /scratch/alpine/$USER/blackhole_data/scripts
-python errorbars.py --ngalaxies=1000 --ndraws=100 --i='/scratch/alpine/$USER/blackhole_data/data/errors.dat' --o='/scratch/alpine/$USER/blackhole_data/data/bhmasses_variances_1000.dat'
+cd /scratch/alpine/mahu8801/blackhole_data/scripts
+python errorbars.py --ngalaxies=1000 --ndraws=100 --i='/scratch/alpine/mahu8801/blackhole_data/data/errors.dat' --o='/scratch/alpine/mahu8801/blackhole_data/data/bhmasses_variances_1000.dat'
