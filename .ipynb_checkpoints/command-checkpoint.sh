cbe2ffaf4f16329c383961ec2d3ed9a04f0df342
maghuber <maggie.c.huber@gmail.com>
2023-05-31 14:31:58 -0600

    added command scripts for 500 and 1000 draws

--- .ipynb_checkpoints/command-checkpoint.sh
+++ .ipynb_checkpoints/command-checkpoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH --time=0:130:00			
+#SBATCH --partition=amilan
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=24:00:00			
 #SBATCH --mail-type=all
 #SBATCH --mail-user=mahu8801@colorado.edu
-#SBATCH --qos=normal	      		
-#SBATCH --partition=amilan		
+#SBATCH --qos=normal	      			
 #SBATCH --output=/scratch/alpine/mahu8801/output/python_%j.out
 
 # Load the python module
@@ -13,4 +15,4 @@ conda activate blackhole_env
 
 # Run Python Script
 cd /scratch/alpine/mahu8801/blackhole_data/scripts
-python errorbars.py --ngalaxies=1000 --ndraws=500 --i='/scratch/alpine/mahu8801/blackhole_data/data/errors.dat' --o='/scratch/alpine/mahu8801/blackhole_data/data/500draws.dat'
+python errorbars.py --ngalaxies=1000 --ndraws=1000 --i='/scratch/alpine/mahu8801/blackhole_data/data/errors.dat' --o='/scratch/alpine/mahu8801/blackhole_data/data/1000draws.dat'
