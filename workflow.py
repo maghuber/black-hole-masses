31fdbc37e3bd97ee4ae0524d2e0ed8d6e83d6058
Maggie Huber <maggiehuber@engr2-13-45-dhcp.int.colorado.edu>
2023-06-23 15:56:39 -0600

    added remote filepaths

--- workflow.py
+++ workflow.py
@@ -7,14 +7,14 @@ for i in range(539):
     else:
         slice2 = (i+1)*1000
         
-    with open('../scripts/command.sh', 'r') as file :
+    with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/command.sh', 'r') as file :
       filedata = file.read()
         
     filedata = filedata.replace('--slice1=0', '--slice1=%i'%(slice1))
     filedata = filedata.replace('--slice2=1000', '--slice2=%i'%(slice2))
     filedata = filedata.replace('1000draws', '%i_%i'%(slice1,slice2))
     
-    with open('../scripts/%i_%i.sh'%(slice1,slice2), 'w') as file:
+    with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh'%(slice1,slice2), 'w') as file:
       file.write(filedata)
         
     subprocess.run("sbatch %i_%i.sh"%(slice1,slice2))
