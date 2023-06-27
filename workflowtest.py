042a8a35c5c8c6792ea0e4123cbbc4ace3baee75
Maggie Huber <maggiehuber@engr2-12-184-dhcp.int.colorado.edu>
2023-06-27 13:55:46 -0600

    workflow update

--- workflowtest.py
+++ workflowtest.py
@@ -1,22 +1,17 @@
 import subprocess 
 
-for i in range(539):
-    slice1 = i*1000
-    if i == 538:
-        slice2 = 538818
-    else:
-        slice2 = (i+1)*1000
+slice1 = 538000
+slice2 = 538818
+with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/command.sh', 'r') as file :
+    filedata = file.read()
         
-    with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/command.sh', 'r') as file :
-      filedata = file.read()
-        
-    filedata = filedata.replace('--slice1=0', '--slice1=%i'%(slice1))
-    filedata = filedata.replace('--slice2=1000', '--slice2=%i'%(slice2))
-    filedata = filedata.replace('1000draws', '%i_%i'%(slice1,slice2))
+filedata = filedata.replace('--slice1=0', '--slice1=%i'%(slice1))
+filedata = filedata.replace('--slice2=1000', '--slice2=%i'%(slice2))
+filedata = filedata.replace('1000draws', '%i_%i'%(slice1,slice2))
     
-    with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh'%(slice1,slice2), 'w') as file:
-      file.write(filedata)
+with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh'%(slice1,slice2), 'w') as file:
+    file.write(filedata)
         
-    subprocess.run("sbatch %i_%i.sh"%(slice1,slice2))
+subprocess.run("sbatch %i_%i.sh"%(slice1,slice2))
     
         
