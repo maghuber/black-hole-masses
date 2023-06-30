3bb89abaf02a9d2289bd10fafba5eda4929d2f16
Maggie Huber <maggiehuber@Maggies-MacBook-Pro.local>
2023-06-30 12:19:26 -0600

    added missing jobs

--- workflow.py
+++ workflow.py
@@ -1,4 +1,5 @@
 import subprocess 
+import os
 
 for i in range(539):
     slice1 = i*1000
@@ -7,14 +8,18 @@ for i in range(539):
     else:
         slice2 = (i+1)*1000
         
-    with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/command.sh', 'r') as file :
-    	filedata = file.read()
-        
-    filedata = filedata.replace('--slice1=0', '--slice1=%i'%(slice1))
-    filedata = filedata.replace('--slice2=1000', '--slice2=%i'%(slice2))
-    filedata = filedata.replace('1000draws', '%i_%i'%(slice1,slice2))
-        
-    with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh'%(slice1,slice2), 'w') as file:
-        file.write(filedata)
+    command = "/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh"%(slice1,slice2)
+    outfile = '/scratch/alpine/mahu8801/blackhole_data/data/%i_%i.dat'%(slice1,slice2)
+    
+    if os.path.exists(outfile) == False:
+        with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/command.sh', 'r') as file :
+        	filedata = file.read()
             
-    subprocess.run(["sbatch", "/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh"%(slice1,slice2)],capture_output=True)
+        filedata = filedata.replace('--slice1=0', '--slice1=%i'%(slice1))
+        filedata = filedata.replace('--slice2=1000', '--slice2=%i'%(slice2))
+        filedata = filedata.replace('data/1000draws.dat', 'data/new/%i_%i.dat'%(slice1,slice2))
+            
+        with open(command, 'w') as file:
+            file.write(filedata)
+                
+        subprocess.run(["sbatch", command],capture_output=True)
