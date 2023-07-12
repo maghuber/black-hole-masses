9548cb18ca631992d505208c48bc44598d537a67
Maggie Huber <maggiehuber@engr2-15-211-dhcp.int.colorado.edu>
2023-07-12 13:41:09 -0600

    running 0 to 1000 jobs

--- workflow.py
+++ workflow.py
@@ -1,25 +1,21 @@
 import subprocess 
 import os
 
-for i in range(47):
-    slice1 = i*1000
-    if i == 46:
-        slice2 = 46663
-    else:
-        slice2 = (i+1)*1000
-        
-    command = "/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh"%(slice1,slice2)
-    outfile = '/scratch/alpine/mahu8801/blackhole_data/data/%i_%i.dat'%(slice1,slice2)
+slice1 = 0
+slice2 = 1000
     
-    if os.path.exists(outfile) == False:
-        with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/command.sh', 'r') as file :
-        	filedata = file.read()
+command = "/scratch/alpine/mahu8801/blackhole_data/scripts/commands/%i_%i.sh"%(slice1,slice2)
+outfile = '/scratch/alpine/mahu8801/blackhole_data/data/%i_%i.dat'%(slice1,slice2)
+
+if os.path.exists(outfile) == False:
+    with open('/scratch/alpine/mahu8801/blackhole_data/scripts/commands/command.sh', 'r') as file :
+        filedata = file.read()
+        
+    filedata = filedata.replace('--slice1=0', '--slice1=%i'%(slice1))
+    filedata = filedata.replace('--slice2=1000', '--slice2=%i'%(slice2))
+    filedata = filedata.replace('data/1000draws.dat', 'data/new/%i_%i.dat'%(slice1,slice2))
+        
+    with open(command, 'w') as file:
+        file.write(filedata)
             
-        filedata = filedata.replace('--slice1=0', '--slice1=%i'%(slice1))
-        filedata = filedata.replace('--slice2=1000', '--slice2=%i'%(slice2))
-        filedata = filedata.replace('data/1000draws.dat', 'data/new/%i_%i.dat'%(slice1,slice2))
-            
-        with open(command, 'w') as file:
-            file.write(filedata)
-                
-        subprocess.run(["sbatch", command],capture_output=True)
+    subprocess.run(["sbatch", command],capture_output=True)
