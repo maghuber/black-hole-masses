ae4109f2e430b0d57bdb50e39e80024e1f586d69
Maggie Huber <maggiehuber@engr2-13-21-dhcp.int.colorado.edu>
2023-10-11 13:17:11 -0600

    inferred bulge mass calculation

--- .ipynb_checkpoints/mbulge_inf-checkpoint.py
+++ .ipynb_checkpoints/mbulge_inf-checkpoint.py
@@ -13,8 +13,6 @@ parser.add_argument("--slice2",type=int,default=1000,
                     help="upper slice of galaxy table")
 parser.add_argument("--ndraws",type=int,
                     help="number of random draws")
-parser.add_argument("--relation",type=str,default='mconnellma',
-                    help="which scaling relation to use")
 parser.add_argument("--i",type=str,
                     help="input file")
 parser.add_argument("--o",type=str,
@@ -25,34 +23,14 @@ n_draws = args.ndraws
 n_galaxies = args.slice2 - args.slice1
 in_file = args.i
 out_file = args.o
-relation = args.relation
 
-if relation == 'mconnellma':
-    # M-sigma for all galaxies
-    alpha_s = 8.32
-    e_alpha_s = 0.05
-    beta_s = 5.64
-    e_beta_s = 0.32
-    scatter_s = 0.38
-    # M-Mbulge for dynamical masses
-    alpha_b = 8.46
-    e_alpha_b = 0.08
-    beta_b = 1.05
-    e_beta_b = 0.11
-    scatter_b = 0.34
-if relation == 'kormendyho':
-    # M-sigma
-    alpha_s = 8.49
-    e_alpha_s = 0.049
-    beta_s = 4.377
-    e_beta_s = 0.29
-    scatter_s = 0.29
-    # M-Mbulge
-    alpha_b = 8.69
-    e_alpha_b = 0.04
-    beta_b = 1.16
-    e_beta_b = 0.06
-    scatter_b = 0.29
+# M-Mbulge for dynamical masses
+alpha_b = 8.46
+e_alpha_b = 0.08
+beta_b = 1.05
+e_beta_b = 0.11
+scatter_b = 0.34
+
 # constants
 Msun = 1.988435e30
 kpctokm = 3.086e16
@@ -84,14 +62,6 @@ def make_distribution_normal(quantity,sigma,s):
     return errordist
 
 # calculate the black hole masses
-def Mbh_sigma(Mtotal,n,r_e,a,b):
-    Kstar = 0.577*(73.32/(10.465+(n-0.94)**2)+0.954)
-    rad = r_e*kpctokm
-    M = Mtotal*Msun
-    sigma = np.sqrt((G*M)/(Kstar*rad))
-    logMbh = a + b*np.log10(sigma/200)
-    logMbh_scatter = np.random.normal(logMbh, scatter_s)
-    return logMbh_scatter
 
 def Mbh_bulge(Mbulge,a,b):
     M = Mbulge*Msun
@@ -99,29 +69,28 @@ def Mbh_bulge(Mbulge,a,b):
     logMbh_scatter = np.random.normal(logMbh, scatter_b)
     return logMbh_scatter
 
+def Mbulge_inf(logMt,g,r):
+    color_cut = 0.06*logMt-0.01
+    gr_color = abs(g-r)
+    if gr_color > color_cut:
+        logMb_inf = 1.*logMt
+    else:
+        logMb_inf = 0.31*logMt
+    return logMb_inf
+
 table = Table.read(in_file,
         format="ascii")
 
 indices = np.s_[args.slice1:args.slice2]
     
 objid = np.asarray(table['objID'][indices],dtype=str)
-logMb = np.asarray(table['logMb'][indices],dtype=float)
-sig1_Mb = np.asarray(table['sig1_Mb'][indices],dtype=float)
-sig2_Mb = np.asarray(table['sig2_Mb'][indices],dtype=float)
 sig1_Mt = np.asarray(table['sig1_Mt'][indices],dtype=float)
 sig2_Mt = np.asarray(table['sig2_Mt'][indices],dtype=float)
 logMt = np.asarray(table['logMt'][indices],dtype=float)
-ng = np.asarray(table['ng'][indices],dtype=float)
-e_ng = np.asarray(table['e_ng'][indices],dtype=float)
-Rhl = np.asarray(table['Rhlr'][indices],dtype=float)
-e = np.asarray(table['e'][indices],dtype=float)
-e_e = np.asarray(table['e_e'][indices],dtype=float)
-false_disk = np.asarray(table['false_disk'][indices],dtype=int)
+gg2d = np.asarray(table['gg2d'][indices],dtype=float)
+rg2d = np.asarray(table['rg2d'][indices],dtype=float)
 
-logMb_dist = make_distribution(logMb,sig1_Mb,sig2_Mb,n_draws)
 logMt_dist = make_distribution(logMt,sig1_Mt,sig2_Mt,n_draws)
-ng_dist = make_distribution_normal(ng,e_ng,n_draws)
-e_dist = make_distribution_normal(e,e_e,n_draws)
 
 # make coefficient distributions
 alpha_s_dist = make_distribution_normal(np.full((n_galaxies,),alpha_s),np.full((n_galaxies,),e_alpha_s),n_draws)
@@ -130,39 +99,29 @@ alpha_b_dist = make_distribution_normal(np.full((n_galaxies,),alpha_b),np.full((
 beta_b_dist = make_distribution_normal(np.full((n_galaxies,),beta_b),np.full((n_galaxies,),e_beta_b),n_draws)
 
 # make black hole mass distributions
-logMbh_s_dist = np.zeros((n_galaxies,n_draws))
+logMb_inf_dist = np.zeros((n_galaxies,n_draws))
 for i in np.arange(n_galaxies):
     for j in np.arange(n_draws):
-        alpha = alpha_s_dist[i][j]
-        beta = beta_s_dist[i][j]
-        r_e = Rhl[i]*np.sqrt(1-e[i])
-        if false_disk[i] == 1:
-            Mtot = 10**(logMb_dist[i][j])
-        else:
-            Mtot = 10**(logMt_dist[i][j])
-        logMbh_s_dist[i][j] = Mbh_sigma(Mtot,ng[i],r_e,alpha,beta)
+        gg = gg2d[i]
+        rg = rg2d[i]
+        Mt = logMt_dist[i][j]
+        logMb_inf_dist[i][j] = Mbulge_inf(Mt,gg,rg)
         
 logMbh_b_dist = np.zeros((n_galaxies,n_draws))
 for i in np.arange(n_galaxies):
     for j in np.arange(n_draws):
         alpha = alpha_b_dist[i][j]
         beta = beta_b_dist[i][j]
-        Mbulge = 10**logMb_dist[i][j]
+        Mbulge = 10**logMb_inf_dist[i][j]
         logMbh_b_dist[i][j] = Mbh_bulge(Mbulge,alpha,beta)
 
-logMbh_s_median = np.zeros(n_galaxies)
-logMbh_s_err = np.zeros(n_galaxies)
 logMbh_b_median = np.zeros(n_galaxies)
 logMbh_b_err = np.zeros(n_galaxies)
 for i in np.arange(n_galaxies):
-    logMbh_s_median[i] = np.around(np.median(logMbh_s_dist[i]),3)
     logMbh_b_median[i] = np.around(np.median(logMbh_b_dist[i]),3)
-    logMbh_s_err[i] = np.around(np.std(logMbh_s_dist[i]),3)
     logMbh_b_err[i] = np.around(np.std(logMbh_b_dist[i]),3)
 
 arr = {'objID' : objid,
-       'logMbh_s': logMbh_s_median,
-       'err_logMbh_s': logMbh_s_err,
        'logMbh_b': logMbh_b_median,
        'err_logMbh_b': logMbh_b_err}
 table = Table(arr)
